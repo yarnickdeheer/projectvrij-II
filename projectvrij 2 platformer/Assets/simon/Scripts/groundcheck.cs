6447f148e71e851fd9199f31bd2a96455aa5ccab
Smos Bois <simondeb1505@gmail.com>
2021-04-27 00:45:54 +0200

    3D focus - beginsels, base gameplay

---
+++ projectvrij 2 platformer/Assets/simon/Scripts/groundcheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundcheck : MonoBehaviour
+{
+    public bool groundTrigger = false;
+    private void OnTriggerStay(Collider other)
+    {
+        groundTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        groundTrigger = false;
+    }
+}
