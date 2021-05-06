83f1ad0f9e769e507537cddea2b374fa4d7865fc
yarnick <yarnickschool@gmail.com>
2021-05-06 20:30:33 +0200

    fmod

---
+++ projectvrij 2 platformer/Assets/yarnick/scripts/LockOnTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOnTarget : MonoBehaviour
+{
+
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+        if (dist < 30)
+        {
+            transform.LookAt(target.transform.position);
+        }
+    }
+}
